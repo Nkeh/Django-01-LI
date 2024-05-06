47a2886472a932c86f2ca24329ffe01adf21bc94
Nkeh <bonyankeh@gmail.com>
2024-05-06 12:46:45 +0100

    Created a base template and static files

--- notes/urls.py
+++ notes/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.NotesListView.as_view()),
-    path('<int:pk>/', views.NotesDetailView.as_view())
+    path('', views.NotesListView.as_view(), name="notes.list"),
+    path('<int:pk>/', views.NotesDetailView.as_view(), name="notes.detail")
 ]
 
